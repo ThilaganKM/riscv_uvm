9af936766193f6752a83d6668f7dcf635837a18b
Thilagan <thilaganmanivannan@gmail.com>
2026-02-23 19:40:07 +0530

    added pc interface

---
+++ tb/interfaces/pc_if.sv
@@ -0,0 +1,22 @@
+interface pc_if(input logic clk);
+
+    logic reset;
+    logic en;
+    logic [31:0] PCNext;
+    logic [31:0] PC;
+
+    modport DRIVER (
+        output reset,
+        output en,
+        output PCNext,
+        input  PC
+    );
+
+    modport MONITOR (
+        input reset,
+        input en,
+        input PCNext,
+        input PC
+    );
+
+endinterface
