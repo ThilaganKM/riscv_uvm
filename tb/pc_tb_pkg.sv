753a08e48fee7aef1256c38cf1cbd013c97c7749
Thilagan <thilaganmanivannan@gmail.com>
2026-02-25 20:46:39 +0530

    fix

--- tb/pc_tb_pkg.sv
+++ tb/pc_tb_pkg.sv
@@ -5,7 +5,6 @@ package pc_tb_pkg;
 
   `include "uvm_macros.svh"
 
-  `include "interfaces/pc_if.sv"
   `include "sequence/pc_base_sequence.sv"
   `include "sequencer/pc_sequencer.sv"
   `include "driver/pc_driver.sv"
