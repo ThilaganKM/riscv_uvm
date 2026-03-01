bb3b60c71790d7cc631034a24cac9bc4d3637b77
Thilagan <thilaganmanivannan@gmail.com>
2026-03-01 22:02:08 +0530

    directional tests

--- rtl/rvhazard_dbg.sv
+++ rtl/rvhazard_dbg.sv
@@ -19,12 +19,10 @@ module rvhazard_dbg(
     assign commit_pc    = core.PCPlus4W - 32'd4;
     always @(posedge clk) begin
         if (core.RegWriteW && core.rdW == 4) begin
-            $display("SUB DEBUG: PC=%h SrcA=%0d SrcB=%0d ALUControl=%0d Result=%0d",
-                    commit_pc,
-                    core.SrcAE,
-                    core.SrcBE,
-                    core.ALUControlE,
-                    core.ResultW);
+            $display("SUB DEBUG: instr=%h funct3=%b funct7b5=%b",
+                    core.InstrD,
+                    core.InstrD[14:12],
+                    core.InstrD[30]);
         end
     end
 
